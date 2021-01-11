b143eb0b411c78ab7bd7b82f4038318114492203
wrecker3000 <73797885+wrecker3000@users.noreply.github.com>
2021-01-12 02:56:52 +0530

    FINAL COMMIT

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -499,7 +499,7 @@ class GoogleDriveHelper:
                 if SHORTENER is not None and SHORTENER_API is not None:
                     url = f"https://drive.google.com/drive/folders/{file.get('id')}"
                     surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                    msg += f'⁍ <a href={surl}>{file.get('name')}📁</a>'
+                    msg += f"⁍ <a href={surl}>{file.get('name')}📁</a>"
                 else:
                     msg += f"⁍ <a href='https://drive.google.com/drive/folders/{file.get('id')}'>{file.get('name')}📁" \
                         f"</a>"
