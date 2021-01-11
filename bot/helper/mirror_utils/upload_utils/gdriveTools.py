fe2f7123c30e422018ab7d42579fb8601fd0f14c
wrecker3000 <73797885+wrecker3000@users.noreply.github.com>
2021-01-12 02:17:55 +0530

    Add files via upload

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -488,6 +488,23 @@ class GoogleDriveHelper:
             return ""
         
         for file in response.get('files', []):
+            if file.get('mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
+                if SHORTENER is not None and SHORTENER_API is not None:
+                    url = f"https://drive.google.com/drive/folders/{file.get('id')}"
+                    surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
+                    msg += f'<a href={surl}>{file.get('name')}📁</a>'
+                else:
+                    msg += f"<a href='https://drive.google.com/drive/folders/{file.get('id')}'>{file.get('name')}📁" \
+                        f"</a>"
+            else:
+                url = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
+                if SHORTENER is not None and SHORTENER_API is not None:
+                    url = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
+                    surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
+                    msg += f"<a href={surl}</a>📄 ({get_readable_file_size(int(file.get('size')))})"
+                else:
+                    msg += f"<a href='https://drive.google.com/uc?id={file.get('id')}" \
+                       f"&export=download'>{file.get('name')}</a>📄 ({get_readable_file_size(int(file.get('size')))})"
             if INDEX_URL is not None:
                 iurl = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
                 if SHORTENER is not None and SHORTENER_API is not None:
@@ -495,22 +512,5 @@ class GoogleDriveHelper:
                     iurl = siurl
                 msg += f'⁍ <a href="{iurl}">INDEX LINK</a>'
                 msg += '\n'
-            if file.get('mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
-                if SHORTENER is not None and SHORTENER_API is not None:
-                    url = f"https://drive.google.com/drive/folders/{file.get('id')}"
-                    surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                    msg += f'📁 <a href={surl}</a>'
-                else:
-                    msg += f"📁<a href='https://drive.google.com/drive/folders/{file.get('id')}'>{file.get('name')}" \
-                        f"</a>"
-            else:
-                url = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
-                if SHORTENER is not None and SHORTENER_API is not None:
-                    url = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
-                    surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                    msg += f"📄 <a href={surl}</a>({get_readable_file_size(int(file.get('size')))})"
-                else:
-                    msg += f"📄 <a href='https://drive.google.com/uc?id={file.get('id')}" \
-                       f"&export=download'>{file.get('name')}</a>({get_readable_file_size(int(file.get('size')))})"
             msg += '\n \n'
         return msg
